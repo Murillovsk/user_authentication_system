922b5abe68ad56cd0df34e07360721f1e7855713
Murillovsk <dev.murillovsk@gmail.com>
2024-07-22 19:25:16 -0300

    auth_sistem

---
+++ task/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+
+# Home do projeto
+def home(request):
+    return render(request, "home.html")
+
+
+def signup(request):
+    if request.method == "GET":
+        return render(request, "signup.html", {"form": UserCreationForm()})
+    else:
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.create_user(
+                    username=request.POST["username"],
+                    password=request.POST["password1"],
+                )
+                user.save()
+                login(request, user)  # type: ignore
+                return redirect("tasks")
+
+            except:
+                return render(
+                    request,
+                    "signup.html",
+                    {"form": UserCreationForm(), "error": "Usuário já existe!"},
+                )
+
+        return render(
+            request,
+            "signup.html",
+            {"form": UserCreationForm(), "error": "As senhas são diferentes."},
+        )
+
+
+def signin(request):
+    if request.method == "GET":
+        return render(request, "signin.html", {"form": AuthenticationForm})
+
+    else:
+        user = authenticate(
+            request,
+            username=request.POST["username"],
+            password=request.POST["password"],
+        )
+
+        if user is None:
+            return render(
+                request,
+                "signin.html",
+                {"form": AuthenticationForm, "error": "Usuário ou senha incorreto."},
+            )
+
+        else:
+            login(request, user)
+            return redirect("tasks")
+
+@login_required
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+@login_required
+def tasks(request):
+    return render(request, "tasks.html")
